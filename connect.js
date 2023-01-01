4a0f9ede09a017de5b7c352a4f2a8b5077580e1c
Abeeb Ahmed <abeebahmed@gmail.com>
2023-01-01 23:37:10 +0100

    mysql setup

--- connect.js
+++ connect.js
@@ -4,8 +4,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "socials",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
 });
+
+db.connect;
