8ccfd506538f21708a7b7ccadf451f337545955e
Abeeb Ahmed <abeebahmed@gmail.com>
2023-01-06 14:34:25 +0100

    code refactor

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ import relationshipRouters from "./routes/relationships.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(
+  cors({ credentials: true, origin: "https://viraly-client.onrender.com/" })
+);
 
 const PORT = process.env.PORT || 8000;
 
